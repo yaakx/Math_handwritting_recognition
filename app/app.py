52133fba766d0756bfee291fec1be1aa01575c9f
Julen Corral <julencv@gmail.com>
2020-04-15 13:54:09 +0200

    Graphs

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from modules.my_class import ImageSolver
 from modules.resolve import MathSolver
 
 app = Flask(__name__)
+ALLOWED_EXTENSIONS = {'png', 'jpg'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/upload-image", methods=["GET", "POST"])
@@ -12,13 +18,25 @@ def upload_image():
     if request.method == "POST":
         if request.files:
             image = request.files["image"]
-            filename = 'static/' + secure_filename(image.filename)
-            image.save(filename)
-            imsol = ImageSolver(filename, os.getcwd() + '/data/models/third_model.h5')
-            msol = MathSolver(imsol.equation, imsol.numbers.white)
-            return render_template('image.html', text=msol)
+            if allowed_file(image.filename):
+                filename = 'static/' + 'eq' + secure_filename(image.filename)
+                image.save(filename)
+                imsol = ImageSolver(filename, os.getcwd() + '/data/models/third_model.h5')
+                msol = MathSolver(imsol.equation)
+                if msol.type == 'F':
+                    return render_template('function.html', array=msol.solution.solution, equation=msol.equation,
+                                           filename=filename)
+                else:
+                    return render_template('image.html', text=msol, equation=msol.equation, filename=filename)
+            else:
+                render_template('test.html')
     return render_template('test.html')
 
 
+@app.route("/", methods=["GET"])
+def index():
+    return redirect(url_for("upload_image"))
+
+
 if __name__ == "__main__":
-    app.run(threaded=False)
+    app.run(threaded=False, port=5001)
--- app/modules/resolve.py
+++ app/modules/resolve.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-from sympy import Symbol
+import numpy as np
+from sympy import Symbol, lambdify, Eq, solveset, S
 from sympy.solvers import solve
 from abc import ABC, abstractmethod
 
@@ -56,31 +56,67 @@ class Equation(MathTypes):
 
 class Function(MathTypes):
     def resolve(self):
-        return self.equation
+        if len(self.symbol) < 3:
+            eq = f'{self.equation.split("=")[0]}-({self.equation.split("=")[1]})'
+            if 'x' in self.symbol:
+                eq = eq.replace('z', 'y')
+            else:
+                eq.replace('z', 'x')
+            x = Symbol('x')
+            y = Symbol('y')
+            f = eval(eq)
+            f_l = lambdify(x, f)
+            self.solution = []
+            lin = np.linspace(-20, 20, 50)
+            print(f)
+            for i in lin:
+                try:
+                    x_sol = solve(f_l(i), y)
+                    for j in x_sol:
+                        self.solution.append([i, j])
+                except:
+                    pass
+        return self.solution
+
+    def derivative(self, symbol):
+        if 'x' in self.symbol:
+            eq = self.equation.replace('z', 'y')
+        else:
+            eq = self.equation.replace('z', 'x')
+        x = Symbol('x')
+        y = Symbol('y')
+        f = Eq(eval(eq.split("=")[0]), eval(eq.split("=")[1]))
+        try:
+            sol = solveset(f, symbol, domain=S.Reals)
+        except:
+            return None
 
     def to_str(self):
-        return self.equation
+        return str(self.equation)
 
 
 class MathSolver(object):
-    def __init__(self, equation, image):
+    def __init__(self, equation):
         self.equation = equation
+        self.type = None
         self.solution = self.classifier()
-        self.image = image
 
     def classifier(self):
         if '=' not in self.equation:
+            self.type = 'C'
             return Calculator(self.equation)
         elif ('x' in self.equation and 'y' not in self.equation and 'z' not in self.equation) or \
                 ('y' in self.equation and 'x' not in self.equation and 'z' not in self.equation) or \
                 ('z' in self.equation and 'y' not in self.equation and 'x' not in self.equation):
+            self.type = 'E'
             return Equation(self.equation, ['x'] if 'x' in self.equation else ['y'] if 'y' in self.equation else ['z'])
         else:
-            return Function(self.equation)
-
-    def image_show(self):
-        plt.imshow(self.image, cmap='gray')
-        plt.show()
+            symbol = []
+            for i in self.equation:
+                if i == 'x' or i == 'y' or i == 'z':
+                    symbol.append(i)
+            self.type = 'F'
+            return Function(self.equation, list(set(symbol)))
 
     def __str__(self):
         return str(self.solution)
@@ -96,10 +132,11 @@ if __name__ == "__main__":
 
     l = os.listdir("../data/images")
     print(l)
-    d = ImageSolver(f'../data/images/{l[1]}', "../data/models/third_model.h5")
-    m = MathSolver(d.equation, d.numbers.white)
-    print(repr(d))
+    d = ImageSolver(f'../data/images/{l[-6]}', "../data/models/third_model.h5")
+    m = MathSolver(d.equation)
+    print(m.solution)
+    """print(repr(d))
     print(d)
     print(m)
     print(repr(m))
-    print(m.solution)
+    print(m.solution)"""
